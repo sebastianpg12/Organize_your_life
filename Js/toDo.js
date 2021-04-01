1946c1812e42f2d43b494c5af69ebc7c08047938
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-04-01 17:48:15 -0500

    toDo list 1.0 completed

--- Js/toDo.js
+++ Js/toDo.js
@@ -5,13 +5,41 @@
         btnNewTask = document.getElementById("btn-add")
     //functions
     var addTask = function(){
-        alert("agg task")
+    var task = taskInput.value,
+        newTask = document.createElement("li"),
+        link = document.createElement("a"),
+        content = document.createTextNode(task);
+
+        if(task == ""){
+            taskInput.setAttribute("placeholder", "Add a task");
+            taskInput.className = "error";
+            return false;
+        }
+        
+        //agg the content of the link
+        link.appendChild(content);
+        //set a atribute href
+        link.setAttribute("href","#");
+        //agg link in the li
+        newTask.appendChild(link);
+        //agg the new task to the list
+        list.appendChild(newTask);
+
+        //The input empty later, of add a task
+        taskInput.value="";
+
+        for (var i = 0; i <= list.children.length -1; i++){
+            list.children[i].addEventListener("click", function(){
+                this.parentNode.removeChild(this);
+            });
+        }
     };
     var checkInput = function(){
-        alert("agg check")
+         taskInput.className = "";
+         taskInput.setAttribute("placeholder", "Add a task");
     };
     var deleteTask= function(){
-        alert("deletetask")
+          this.parentNode.removeChild(this);
     };
     //events
     
