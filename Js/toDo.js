8e91eb5db252572a7b0c06ff31d06c155f3b339e
Sebastián Pulgarín <pulgaringomezsebastian@gmail.com>
2021-04-01 22:34:14 -0500

    Started LocalStorage

--- Js/toDo.js
+++ Js/toDo.js
@@ -1,17 +1,30 @@
 (function(){
     //var
     var list = document.getElementById("list"),
+
         taskInput = document.getElementById("taskInput"),
         btnNewTask = document.getElementById("btn-add");
     
        
     //functions
+    var getLocalStorage = function(){
+        let nameNewTask = JSON.parse(localStorage.getItem("newTask"));
+        let linkTask = JSON.parse(localStorage.getItem("link"));
+        let contentTask = JSON.parse(localStorage.getItem("content"));
+    }
+
     var addTask = function(){
     var task = taskInput.value,
         newTask = document.createElement("li"),
         link = document.createElement("a"),
         content = document.createTextNode(task);
-
+        
+        localStorage.setItem("li", JSON.stringify("li"));
+        localStorage.setItem("a",  JSON.stringify("a"));
+        localStorage.setItem(task, JSON.stringify(task));
+        
+        //var nombre = localStorage.getItem("Nombre");
+        //var apellido = localStorage.getItem("Apellido");
         if(task == ""){
             taskInput.setAttribute("placeholder", "Add a task");
             taskInput.className = "error";
@@ -26,6 +39,8 @@
         newTask.appendChild(link);
         //agg the new task to the list
         list.appendChild(newTask);
+
+
 
         //The input empty later, of add a task
         taskInput.value="";
@@ -47,9 +62,9 @@
     
     //Agg task
     btnNewTask.addEventListener("click", addTask);
-
     //Check input
     taskInput.addEventListener("click", checkInput);
+    //show AllTask
 
     //Deleting elements of the list
     for (var i = 0; i <= list.children.length -1; i++){
